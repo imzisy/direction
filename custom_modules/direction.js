c8d9dcf755b745025efaad6420bc13a9711cb371
Emad zaibak <Emad zaibak>
2017-10-21 15:29:08 +0800

    code polishing

--- custom_modules/direction.js
+++ custom_modules/direction.js
@@ -4,9 +4,9 @@ const Q = require("q");
 const googleMapsClient = require("@google/maps").createClient();
 
 /**
- * Function for sort an array of points by array of 
+ * Function for sort an array of points by array of
  * indexes of original array in sorted order
- * 
+ *
  * @param points
  * @param waypointOrder
  * @return list of sorted Points
@@ -14,7 +14,7 @@ const googleMapsClient = require("@google/maps").createClient();
 function sortPointsByWaypointOrder(points,waypointOrder){
     let sortedPoints = [];
     for (let i = 0; i < waypointOrder.length; i++) {
-        sortedPoints.push(points[waypointOrder[i]]);   
+        sortedPoints.push(points[waypointOrder[i]]);
     }
     return sortedPoints;
 }
@@ -39,8 +39,8 @@ function getDirection(points) {
         }
         if(response.json.status == "OK"){
             return deferred.resolve(response.json);
-        } 
-        deferred.reject(response.json.status);        
+        }
+        deferred.reject(response.json.status);
     });
     return deferred.promise;
 }
